31b9d58c646fc30ccebb4aa8c3dace1913af885d
tmsh <tomasmasak02@gmail.com>
2025-12-15 20:59:01 +0100

    Implement working TCP client

--- ChessGame/TCPClient.cs
+++ ChessGame/TCPClient.cs
@@ -15,20 +15,35 @@ namespace ChessGame
             System.Diagnostics.Debug.WriteLine("TCPClient initialized");
             Task.Run(InitializeClient);
         }
-
-        private async Task InitializeClient()
+        
+        private IPAddress GetServerIP()
         {
+            using UdpClient udpClient = new UdpClient(11001);
+
+            while (true)
+            {
+                var result = udpClient.ReceiveAsync();
+                var message = Encoding.UTF8.GetString(result.Result.Buffer);
+
+                if(message.StartsWith("CLIENTFOUND"))
+                {
+                    return result.Result.RemoteEndPoint.Address;
+                }
+            }
+        }
+
+        public async Task InitializeClient()
+        {
+            IPAddress serverIP = GetServerIP();
             var eom = "<EOM>";
-            IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(Dns.GetHostName());
-            IPAddress localIpAddress = IPAddress.Loopback;
-            IPEndPoint ipEndPoint = new(localIpAddress, 11_000);
+            IPEndPoint ipEndPoint = new(serverIP, 11_000);
 
             using Socket client = new(
                 ipEndPoint.AddressFamily,
                 SocketType.Stream,
                 ProtocolType.Tcp);
             await client.ConnectAsync(ipEndPoint);
-                var message = "d2d4"+eom;
+                var message = "If you see this I'm the best programmer that has ever lived"+eom;
                 var messageBytes = Encoding.UTF8.GetBytes(message);
                 _ = await client.SendAsync(messageBytes, SocketFlags.None);
                 message = message.Replace(eom, "");
